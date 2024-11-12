5fb01cd220748d2e792b6243c38bc3892d6fe0e8
Morris Mukiri <morrismukiri@gmail.com>
2024-11-12 16:19:24 +0200

    Add version 9
    Signed-off-by: Morris Mukiri <morrismukiri@gmail.com>

--- stack/startup.sh
+++ stack/startup.sh
@@ -1,29 +1,48 @@
 #!/bin/sh
-set -ex # fail on any error & print commands as they're run
-if [ "x$MANAGEPY_COLLECTSTATIC" = "xon" ]; then
-	/venv/bin/python manage.py collectstatic --noinput --no-post-process
+# Ensures the script exits on any error and prints commands as they're run
+set -e
+set -x
+
+# Collect static files if enabled
+if [ "${MANAGEPY_COLLECTSTATIC:-off}" = "on" ]; then
+    /venv/bin/python manage.py collectstatic --noinput --no-post-process
 fi
-if [ "x$CLEAR_COMPRESSOR_CACHE" = "xon" ]; then
-	/venv/bin/python clear-compressor-cache.py
+
+# Clear the compressor cache if enabled
+if [ "${CLEAR_COMPRESSOR_CACHE:-off}" = "on" ]; then
+    /venv/bin/python clear-compressor-cache.py
 fi
-if [ "x$MANAGEPY_COMPRESS" = "xon" ]; then
-	/venv/bin/python manage.py compress --extension=".haml" --force -v0
+
+# Compress files if enabled
+if [ "${MANAGEPY_COMPRESS:-off}" = "on" ]; then
+    /venv/bin/python manage.py compress --extension=".haml" --force -v0
 fi
-if [ "x$MANAGEPY_INIT_DB" = "xon" ]; then
-	set +x  # make sure the password isn't echoed to stdout
-	echo "*:*:*:*:$(echo \"$DATABASE_URL\" | cut -d'@' -f1 | cut -d':' -f3)" > $HOME/.pgpass
-	set -x
-	chmod 0600 $HOME/.pgpass
-	/venv/bin/python manage.py dbshell < init_db.sql
-	rm $HOME/.pgpass
+
+# Initialize the database if enabled
+if [ "${MANAGEPY_INIT_DB:-off}" = "on" ]; then
+    # Temporarily stop echoing commands to avoid leaking sensitive information
+    set +x
+    # Configure .pgpass for passwordless PostgreSQL operations
+    echo "*:*:*:*:$(echo "$DATABASE_URL" | cut -d'@' -f1 | cut -d':' -f3)" > $HOME/.pgpass
+    chmod 0600 $HOME/.pgpass
+    set -x
+    /venv/bin/python manage.py dbshell < init_db.sql
+    rm $HOME/.pgpass
 fi
-if [ "x$MANAGEPY_MIGRATE" = "xon" ]; then
-	/venv/bin/python manage.py migrate
+
+# Run database migrations if enabled
+if [ "${MANAGEPY_MIGRATE:-off}" = "on" ]; then
+    /venv/bin/python manage.py migrate
 fi
-if [ "x$MANAGEPY_IMPORT_GEOJSON" = "xon" ]; then
-	echo "Downloading geojson for relation_ids $OSM_RELATION_IDS"
-	/venv/bin/python manage.py download_geojson $OSM_RELATION_IDS
-	/venv/bin/python manage.py import_geojson ./geojson/*.json
-	echo "Imported geojson for relation_ids $OSM_RELATION_IDS"
+
+# Import GeoJSON files if enabled
+if [ "${MANAGEPY_IMPORT_GEOJSON:-off}" = "on" ]; then
+    echo "Downloading GeoJSON for relation_ids: $OSM_RELATION_IDS"
+    /venv/bin/python manage.py download_geojson $OSM_RELATION_IDS
+    /venv/bin/python manage.py import_geojson ./geojson/*.json
+    echo "Imported GeoJSON for relation_ids: $OSM_RELATION_IDS"
 fi
-$STARTUP_CMD
+
+# Execute the command to start the server or other process
+echo "Starting application with command: $STARTUP_CMD"
+exec $STARTUP_CMD
